b1a3c7ad2d7131d2682f87f4c7d4b86c8d1cdd37
blaxcull <blaxcull@gmail.com>
2025-08-13 00:10:24 +0530

    display every keystroke

---
+++ app/components/canvas.tsx
@@ -0,0 +1,49 @@
+'use client'
+import { Application, extend } from '@pixi/react'
+import { Text } from 'pixi.js'
+import { useState, useEffect} from 'react'
+
+extend({ Text})
+
+    
+
+const Canvas = () => {
+
+    const [textArr, setTextArr] = useState([])
+
+
+
+useEffect(() => {
+    const handleKeyDown = (event) => {
+
+              setTextArr((prev) => [...prev, event.key]);
+
+      console.log("Key pressed:", event.key);
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);    
+    
+
+  return (
+    <div>
+      hello
+      <Application width={800} height={600} backgroundColor={0x000000}>
+        <pixiText
+          text={textArr.join('')}
+          x={0}
+          y={0}
+          style={{
+            fill: '#ffffff',
+            fontSize: 36,
+            fontFamily: 'Arial'
+          }}
+        />
+      </Application>
+    </div>
+  )
+}
+
+export default Canvas
+
